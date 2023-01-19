3ab14cdaacbb402cec815efbaa635391011e941c
pkkulhari <praveen.kulhari07@gmail.com>
2023-01-19 21:03:19 +0530

    deploy: change vite settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,15 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target:
+          mode == 'development'
+            ? 'http://127.0.0.1:8000'
+            : 'https://budget-tracker-986o.onrender.com',
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+}))
