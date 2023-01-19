2e42bf157a19dcaeb9313b95a48d345d7c6cde8d
pkkulhari <praveen.kulhari07@gmail.com>
2023-01-19 20:19:41 +0530

    fix: django debug mode logic

--- backend/budget_tracker/settings.py
+++ backend/budget_tracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (
-    os.environ.get("DJANGO_DEBUG") != "True"
+    os.environ.get("DJANGO_DEBUG", "True") == "True"
 )  # Just set default to true for easy local installation
 
 ALLOWED_HOSTS = ["*"]
